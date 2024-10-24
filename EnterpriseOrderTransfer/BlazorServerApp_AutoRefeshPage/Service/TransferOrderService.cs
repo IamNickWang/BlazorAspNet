fa02756ca6eb33f2e656184a1ecc7acdf2515170
Nick Wang <nickwang33@hotmail.com>
2024-10-24 15:15:09 -0700

    Add files via upload

--- EnterpriseOrderTransfer/BlazorServerApp_AutoRefeshPage/Service/TransferOrderService.cs
+++ EnterpriseOrderTransfer/BlazorServerApp_AutoRefeshPage/Service/TransferOrderService.cs
@@ -6,15 +6,20 @@ namespace BlazorServerApp_AutoRefeshPage.Service
     public class TransferOrderService : ITransferOrder
     {
         private readonly DatabaseContext _dbt;
+        private readonly IConfiguration _config;
 
-        public TransferOrderService(DatabaseContext ctx)
+
+        public TransferOrderService(DatabaseContext ctx, IConfiguration config)
         {
             _dbt = ctx;
+            _config = config;
         }
         public async Task<List<erpTransferOrder>> GetTransferOrders()
         {
+            string sql = "SELECT * FROM erp.netsuiteTransferOrder";
+            string connectionString = _config.GetConnectionString("conn");
 
-            return await _dbt.erpTransferOrder.ToListAsync();
+            return await _dbt.LoadData<erpTransferOrder>(sql, connectionString);
 
         }
     }
