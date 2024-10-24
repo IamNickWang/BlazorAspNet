b63fd4d7132994ac801c9424b236e5edcfbadfa7
Nick Wang <nickwang33@hotmail.com>
2024-10-24 15:13:55 -0700

    Add files via upload

--- EnterpriseOrderTransfer/TransferOrderRolling/Service/TransferOrderService.cs
+++ EnterpriseOrderTransfer/TransferOrderRolling/Service/TransferOrderService.cs
@@ -1,21 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TransferOrderRolling.Model;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+
 
 namespace TransferOrderRolling.Service
 {
     public class TransferOrderService : ITransferOrder
     {
         private readonly DatabaseContext _dbt;
+        private readonly IConfiguration _config;
 
-        public TransferOrderService(DatabaseContext ctx) 
+
+        public TransferOrderService(DatabaseContext ctx, IConfiguration config) 
         {
             _dbt = ctx;
+            _config = config;
         }
         public async Task<List<erpTransferOrder>> GetTransferOrders()
         {
-            
-            return await _dbt.erpTransferOrder.ToListAsync();
+            string sql = "SELECT * FROM erp.netsuiteTransferOrder";
+            string connectionString = _config.GetConnectionString("conn");
 
+            return await _dbt.LoadData<erpTransferOrder>(sql, connectionString);
+            
         }
+
     }
 }
