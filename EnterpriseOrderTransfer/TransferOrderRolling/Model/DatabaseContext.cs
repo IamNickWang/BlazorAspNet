b999435e4e0385ef7e842269d6c330001d4bcaf1
Nick Wang <nickwang33@hotmail.com>
2024-10-24 15:12:18 -0700

    Add files via upload

--- EnterpriseOrderTransfer/TransferOrderRolling/Model/DatabaseContext.cs
+++ EnterpriseOrderTransfer/TransferOrderRolling/Model/DatabaseContext.cs
@@ -1,15 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace TransferOrderRolling.Model
 {
     public class DatabaseContext : DbContext
     {
+        private readonly DatabaseContext _dbt;
+        private readonly IConfiguration _config;
+
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
 
         }
 
-        public DbSet<erpTransferOrder> erpTransferOrder { get; set; }
+        //public DbSet<erpTransferOrder> erpTransferOrder { get; set; }
+
+        public async Task<List<erpTransferOrder>> LoadData<erptransferOrder>(string sql, string conn)
+        {
+
+            using (IDbConnection connection = new SqlConnection(conn))
+            {
+                var data = await connection.QueryAsync<erpTransferOrder>(sql);
+
+                return data.ToList();
+            }
+
+        }
 
     }
    
